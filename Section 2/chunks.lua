0c4fc79b062f6d80f924809610330c19a0c43b07
Raspaholic <tonyrasp31@gmail.com>
2022-12-09 13:17:42 -0600

    Section 2 Lua

---
+++ Section 2/chunks.lua
@@ -0,0 +1,7 @@
+-- chunks: each piece of code that Lua executes
+-- dofile: another way to run chunks that executes the file immediately 
+function twice(x)
+  return 2.0 * x
+end
+
+function norm(x, y)
